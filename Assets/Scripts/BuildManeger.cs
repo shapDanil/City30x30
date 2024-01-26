235ae1c4fa35eff733f7b535daeca6bb41aeaf96
shapDanil <d.shapovalov@g.nsu.ru>
2024-01-26 12:48:04 +0700

    Задержка строительства
    Добавлена фиксированная задержка строительства зданий

--- Assets/Scripts/BuildManeger.cs
+++ Assets/Scripts/BuildManeger.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
+using System.Collections;
 
 public class BuildManager : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
     [SerializeField] private GameObject _currentTypeBuilding;
 
+    private WaitForSeconds _wait;
+
+    private void Awake()
+    {
+        _wait = new WaitForSeconds(3f);
+    }
     private void OnEnable()
     {
         Cell.OnMouseDowned += Build;
@@ -17,12 +24,19 @@ public class BuildManager : MonoBehaviour
     }
     private void Build(GameObject cellGameObject)
     {
-        Instantiate(_currentTypeBuilding, cellGameObject.transform.position, Quaternion.identity,cellGameObject.transform);
+        StartCoroutine(StartConstruction(cellGameObject));
     }
 
     public void SetCurrentTypeBuilding(GameObject gameObject)
     {
         _currentTypeBuilding = gameObject;
+       // _wait = Building.GetWait();
     }
-    
+
+    IEnumerator StartConstruction(GameObject cellGameObject)
+    {
+        yield return _wait;
+        Instantiate(_currentTypeBuilding, cellGameObject.transform.position, Quaternion.identity, cellGameObject.transform);
+    }
+
 }
